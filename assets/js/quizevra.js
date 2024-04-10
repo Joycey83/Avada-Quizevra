a5bace17e925e7cb5204792828f79ae1d4d39202
Joyce Cheung <Jcheung8385@gmail.com>
2024-04-10 10:06:12 +0100

    changed it to 2 question as 4 is too much for showing the score feature

--- assets/js/quizevra.js
+++ assets/js/quizevra.js
@@ -13,7 +13,7 @@ let shuffledQuestions = []; //empty array to hold shuffled selected questions
 
 function handleQuestions() {
   //function to shuffle and push 25 questions to shuffledQuestions array
-  while (shuffledQuestions.length <= 3) {
+  while (shuffledQuestions.length <= 2) {
     let random = questions[Math.floor(Math.random() * questions.length)];
     if (!shuffledQuestions.includes(random)) {
       shuffledQuestions.push(random);
